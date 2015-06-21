e84452d817cb37e8027034a8c2d7f960329a460a
drnida <rnida@mac.local>
2015-06-21 16:18:22 -0700

    jsonerific

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -1,16 +1,17 @@
-#import tweepy
+# pip install Twython to get the twython module
+
 import time
 from twython import TwythonStreamer
 from twython import Twython
-import Keys
+import keys
 
 MAXFILESIZE = 1024**3
 
 
-consumer_key = Keys.consumer_key
-consumer_secret = Keys.consumer_secret 
-access_token = Keys.access_token 
-access_token_secret = Keys.access_token_secret 
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret 
+access_token = keys.access_token 
+access_token_secret = keys.access_token_secret 
 
 count = 0
 
@@ -25,7 +26,8 @@ def fileOut(data):
 
     f = open('ouput_twitter.json' , 'a')
 
-    f.write(str(data['text'].encode('utf-8')) + " | ")
+    f.write(str(data))
+    f.write(",\n")
     #print f.tell()
     if(f.tell() > MAXFILESIZE): #If greater than x file size quit
         f.close()
@@ -74,10 +76,17 @@ stream = MyStreamer(consumer_key, consumer_secret,
 
 print "Running"
 
+
+with open('ouput_twitter.json' , 'a') as f:
+    f.write('{\n\t"tweets": [\n')
+
+
 try:
     stream.statuses.sample()
-except:
-    pass
+except(KeyboardInterrupt):
+    with open('ouput_twitter.json' , 'a') as f:
+        f.write("{}\n\t]\n}\n")
+
 
 
 
